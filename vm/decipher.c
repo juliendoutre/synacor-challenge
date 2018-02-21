6bc45a6484fcc704ccb65e94e924f7a22985832c
Julien Doutre <julien.doutre@student.ecp.fr>
2018-02-21 16:47:21 +0100

    immproved decipher function

--- vm/decipher.c
+++ vm/decipher.c
@@ -33,34 +33,46 @@ void decipher(uint16_t *memory)
             }
 	    }
 
-        fprintf(fp, "%d | ", ID);
-        ID += 1;
         switch(memory[i])
         {
             case 0:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "halt: ");
                 i += 1;
+                fprintf(fp, "\n");
                 break;
             case 1:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "set: ");
                 printVar(fp, var1);
                 fprintf(fp, " = ");
                 printVar(fp, var2);
+                fprintf(fp, "\n");
                 i += 3;
                 break;
             case 2:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "push: push(");
                 printVar(fp, var1);
                 fprintf(fp, ");");
+                fprintf(fp, "\n");
                 i += 2;
                 break;
             case 3:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "pop: ");
                 printVar(fp, var1);
                 fprintf(fp, " = pop();");
                 i += 2;
+                fprintf(fp, "\n");
                 break;
             case 4:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "eq: if (");
                 printVar(fp, var2);
                 fprintf(fp, " == ");
@@ -70,9 +82,12 @@ void decipher(uint16_t *memory)
                 fprintf(fp, " == 1 } else { ");
                 printVar(fp, var1);
                 fprintf(fp, " == 0 }");
+                fprintf(fp, "\n");
                 i += 5;
                 break;
             case 5:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "gt: if (");
                 printVar(fp, var2);
                 fprintf(fp, " > ");
@@ -82,129 +97,172 @@ void decipher(uint16_t *memory)
                 fprintf(fp, " == 1 } else { ");
                 printVar(fp, var1);
                 fprintf(fp, " == 0 }");
+                fprintf(fp, "\n");
                 i += 5;
                 break;
             case 6:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "jmp: go to memory[");
                 printVar(fp, var1);
                 fprintf(fp, "]");
+                fprintf(fp, "\n");
                 i += 2;
                 break;
             case 7:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "jt: if (");
                 printVar(fp, var1);
                 fprintf(fp, " != 0 { go to memory[");
                 printVar(fp, var2);
                 fprintf(fp, "] }");
+                fprintf(fp, "\n");
                 i += 3;
                 break;
             case 8:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "jt: if (");
                 printVar(fp, var1);
                 fprintf(fp, " == 0 { go to ");
                 printVar(fp, var2);
                 fprintf(fp, " }");
+                fprintf(fp, "\n");
                 i += 3;
                 break;
             case 9:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "add: ");
                 printVar(fp, var1);
                 fprintf(fp, " = ");
                 printVar(fp, var2);
                 fprintf(fp, " + ");
                 printVar(fp, var3);
+                fprintf(fp, "\n");
                 i += 4;
                 break;
             case 10:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "mult: ");
                 printVar(fp, var1);
                 fprintf(fp, " = ");
                 printVar(fp, var2);
                 fprintf(fp, " * ");
                 printVar(fp, var3);
+                fprintf(fp, "\n");
                 i += 4;
                 break;
             case 11:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "mod: ");
                 printVar(fp, var1);
                 fprintf(fp, " = ");
                 printVar(fp, var2);
                 fprintf(fp, " %% ");
                 printVar(fp, var3);
+                fprintf(fp, "\n");
                 i += 4;
                 break;
             case 12:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "and: ");
                 printVar(fp, var1);
                 fprintf(fp, " = ");
                 printVar(fp, var2);
                 fprintf(fp, " and ");
                 printVar(fp, var3);
+                fprintf(fp, "\n");
                 i += 4;
                 break;
             case 13:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "or: ");
                 printVar(fp, var1);
                 fprintf(fp, " = ");
                 printVar(fp, var2);
                 fprintf(fp, " or ");
                 printVar(fp, var3);
+                fprintf(fp, "\n");
                 i += 4;
                 break;
             case 14:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "not: ");
                 printVar(fp, var1);
                 fprintf(fp, " = ~");
                 printVar(fp, var2);
+                fprintf(fp, "\n");
                 i += 3;
                 break;
             case 15:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "rmem: ");
                 printVar(fp, var1);
                 fprintf(fp, " = memory[");
                 printVar(fp, var2);
                 fprintf(fp, "]");
+                fprintf(fp, "\n");
                 i += 3;
                 break;
             case 16:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "wmem: memory[");
                 printVar(fp, var1);
                 fprintf(fp, "] = ");
                 printVar(fp, var2);
+                fprintf(fp, "\n");
                 i += 3;
                 break;
             case 17:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "call: push(");
                 printVar(fp, i + 2);
                 fprintf(fp, ");\n");
                 fprintf(fp, "go to memory[");
                 printVar(fp, var1);
                 fprintf(fp, "]");
+                fprintf(fp, "\n");
                 i += 2;
                 break;
             case 18:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "ret: go to memory[pop()];");
+                fprintf(fp, "\n");
                 i += 1;
                 break;
             case 19:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "out: printf(");
                 printVar(fp, var1);
                 fprintf(fp, ");");
+                fprintf(fp, "\n");
                 i += 2;
                 break;
             case 20:
+                fprintf(fp, "%d | ", ID);
+                ID += 1;
                 fprintf(fp, "in: ");
                 printVar(fp, var1);
                 fprintf(fp, " = getchar();");
+                fprintf(fp, "\n");
                 i += 2;
                 break;
             default:
                 i += 1;
-                fprintf(fp, "noop: ");
                 break;
         }
-        fprintf(fp, "\n");
-        i +=1;
     }
     fclose(fp);
 }
